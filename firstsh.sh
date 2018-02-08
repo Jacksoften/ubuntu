7507a5691104d2cedde279ae59a2f6c07661ad6b
yunzhe li <yunzheli@yunzhes-mbp.lan>
2018-02-08 01:01:47 -0800

    varaibles

--- firstsh.sh
+++ firstsh.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
 echo "hello world"
